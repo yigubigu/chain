d26c254e88542454658d677cf406b387be47e21b
ameets <amita@chain.com>
2016-12-08 15:44:18 -0800

    new file for raft.go libraries

---
+++ core/raft/rand.go
@@ -0,0 +1,16 @@
+package raft
+
+import (
+"crypto/rand"
+)
+
+func randID() []bytes {
+	c := 16 // prevent collisions
+	b := make([]byte, c)
+	_, err := rand.Read(b)
+	if err != nil {
+    		panic(err)
+	}
+	return b
+}
+
